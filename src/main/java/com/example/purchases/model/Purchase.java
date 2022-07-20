93f0f291bd24c2f621de4d8f645d2b5205493443
Vladimir Malzam <v.malzam@gmail.com>
2022-07-20 12:06:06 +0300

    Create Entity classes

---
+++ src/main/java/com/example/purchases/model/Purchase.java
@@ -0,0 +1,37 @@
+package com.example.purchases.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Purchase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @OneToOne
+    private Buyer buyer;
+    @OneToOne
+    private Product product;
+    private Integer count;
+    private BigDecimal amount;
+    private LocalDate date;
+
+    // Необходимость переопределения ToString, Equals и HashCode в рамках этого задания - дискуссионная тема
+    // Читал о разных подходах. Интересны принятые у вас практики по этому вопросу для Entity-классов.
+}
+
+
