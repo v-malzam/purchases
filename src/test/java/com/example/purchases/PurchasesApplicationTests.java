f669b3d8d2c1c50426ef3cdf089e58c95778f623
Vladimir Malzam <v.malzam@gmail.com>
2022-07-20 10:09:09 +0300

    init

---
+++ src/test/java/com/example/purchases/PurchasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purchases;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
